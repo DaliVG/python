6ac23a7eef22d682b27f9c45462ba3274bbcf84d
Daliborka Vidic Gutierrez <dvidic@MacBook-Air-de-Daliborka.local>
2026-02-18 17:34:23 +0000

    feat(): while

--- objets/12-entrada.py
+++ objets/12-entrada.py
@@ -1,14 +1,17 @@
 #nombre = input('Cual es tu nombre: \r\n')
 
 #print(f'Hola {nombre}')
+preguntar = True;
 
+while preguntar:
 # con if else etc
 
-edad = input('Cual es tu edad: \r\n')
-edad = int(edad)
-print(f'Tienes {edad} años')
+    edad = input('Cual es tu edad: \r\n')
+    edad = int(edad)   
+    print(f'Tienes {edad} años')    
 
-if edad >= 18:
-    print('Votas')
-else:
-    print('No votas')
+    if edad >= 18:
+        print('Votas')
+    else:
+        preguntar = False
+        print('No votas')
